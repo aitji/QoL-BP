0017021cbd7e63ec1814f83c3ca9a5a0b860e4c5
aitji <ait.suriya@gmail.com>
2026-03-28 14:26:52 +0700

    small clean up

--- scripts/_helper.js
+++ scripts/_helper.js
@@ -2,23 +2,24 @@ import { EntityItemPickupBeforeEvent, EntityRemoveAfterEvent, system, world } fr
 
 const COOLDOWN_ENTITY = new Map()
 export const addItemCooldown = (entity, cooldown = 40) => {
-    if (COOLDOWN_ENTITY.get(entity.id)) return
-    COOLDOWN_ENTITY.set(entity.id, system.currentTick + cooldown)
+    const id = entity.id
+    if (COOLDOWN_ENTITY.has(id)) return
+    COOLDOWN_ENTITY.set(id, system.currentTick + cooldown)
 }
 
 /** @param {EntityItemPickupBeforeEvent} data */
 export const helper_entityItemPickup = (data) => {
-    const { entity, item } = data
-    if (COOLDOWN_ENTITY.has(entity.id)) {
-        const tick = system.currentTick
-        const cooldown = COOLDOWN_ENTITY.get(entity.id)
-        if (tick < cooldown) data.cancel = true
-        else COOLDOWN_ENTITY.delete(entity.id)
-    }
+    const id = data?.entity?.id
+    if (!id) return
+
+    const cooldown = COOLDOWN_ENTITY.get(id)
+    if (!cooldown || cooldown === undefined) return
+    if (system.currentTick < cooldown) return data.cancel = true
+
+    COOLDOWN_ENTITY.delete(id)
 }
 
 /** @param {EntityRemoveAfterEvent} data */
 export const helper_entityRemove = (data) => {
-    const { removedEntityId } = data
-    if (COOLDOWN_ENTITY.has(removedEntityId)) COOLDOWN_ENTITY.delete(removedEntityId)
+    COOLDOWN_ENTITY.delete(data.removedEntityId)
 }
